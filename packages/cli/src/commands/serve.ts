4d1cbb80a35908c7f1f81661762b233ce306a078
William <willspecker@gmail.com>
2021-04-05 21:34:14 +0200

    specifies some command line options and connects cli with local api

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,8 +1,10 @@
 import { Command } from 'commander'
+import { serve } from 'local-api'
 
 export const serveCommand = new Command()
-  .command('serve')
+  .command('serve [filename]')
   .description('Open a file to edit')
-  .action(() => {
-    console.log('warming up...')
+  .option('-p, --port <number>', 'port to run server on', '4005')
+  .action((filename = 'notebook.js', options: { port: string }) => {
+    serve(parseInt(options.port), filename, '/')
   })
