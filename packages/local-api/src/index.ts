a1768e6a44eae721a9722cdd738449739090bb20
William <willspecker@gmail.com>
2021-04-05 21:47:16 +0200

    adds functionality to calculate file paths in cli serve command

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,5 +1,5 @@
 export const serve = (port: number, filename: string, dir: string) => {
   console.log('serving on port', port)
-  console.log('cells from', filename)
+  console.log('saving/fetching from', filename)
   console.log('in directory', dir)
 }
